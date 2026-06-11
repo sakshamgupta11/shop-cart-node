cf7f46850eae5d29c20b68ac028bfc3fa10ffa29
sakshamgupta11 <sakshamgupta2308@gmail.com>
2026-06-11 15:34:33 +0530

    add pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,8 @@ const path = require("path");
 const PDFDocument = require("pdfkit");
 const product = require('../models/product');
 
+const ITEMS_PER_PAGE = 3;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -36,12 +38,27 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1
+  let totalItems;
+
   Product.find()
+    .countDocuments().then(numProducts => {
+      totalItems = numProducts;
+      return Product.find().skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
+
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        currentPage:page,
+        hasNextPage:ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page>1,
+        nextPage: page+1,
+        previousPage :page -1,
+        lastPage: Math.ceil(totalItems/ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -237,8 +254,8 @@ exports.getInvoice = (req, res, next) => {
 
     })
     pdfDoc.text()
-    pdfDoc.fontSize(18).text(` totalPrice ${totalPrice}`,{
-      align:'center'
+    pdfDoc.fontSize(18).text(` totalPrice ${totalPrice}`, {
+      align: 'center'
     })
 
     pdfDoc.end();
